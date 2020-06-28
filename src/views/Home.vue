aead340bfa111e788af0aad15be9ea70fdcbc3ec
Marc Bouvier <m.bouvier.dev@gmail.com>
2020-06-28 17:49:09 +0200

    base login that just store encoded credentials ready to be passed to neo4j api

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Hi {{user.firstName}}!</h1>
+        <p>You're logged in with Vue.js & Basic HTTP Authentication!!</p>
+  
+        
+        <p>
+            <router-link to="/login">Logout</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            user: {},
+            users: []
+        }
+    },
+    created () {
+        this.user = JSON.parse(localStorage.getItem('user'));
+
+    }
+};
+</script>
